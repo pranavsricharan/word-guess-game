abf50451fe55501852e1482bc0a2f9bddb2ea35c
Pranav Sricharan <pranav.sricharan@gmail.com>
2022-02-06 16:54:59 +0530

    chore: rearrange declaration in Results model

--- src/model/Result.ts
+++ src/model/Result.ts
@@ -1,15 +1,23 @@
-export class Result {
-  private constructor(
-    public readonly answer: Answer,
-    public readonly letters?: LetterResult[]) {}
+export enum AnswerStatus {
+  CORRECT,
+  WRONG
+}
+export class Answer {
+  private constructor(public readonly status: AnswerStatus, public readonly word?: string) {}
 
   public static correct(word: string) {
-    return new Result(Answer.correct(word));
+    return new Answer(AnswerStatus.CORRECT, word);
   }
 
-  public static wrong(letters: LetterResult[], word?: string) {
-    return new Result(Answer.wrong(word), letters);
+  public static wrong(word?: string) {
+    return new Answer(AnswerStatus.WRONG, word);
   }
+}
+
+export enum LetterStatus {
+  CORRECT_POSITION,
+  INCORRECT_POSITION,
+  NOT_PRESENT
 }
 
 export class LetterResult {
@@ -28,27 +36,18 @@ export class LetterResult {
   }
 }
 
-export class Answer {
-  private constructor(public readonly status: AnswerStatus, public readonly word?: string) {}
+export class Result {
+  private constructor(
+    public readonly answer: Answer,
+    public readonly letters?: LetterResult[]) {}
 
   public static correct(word: string) {
-    return new Answer(AnswerStatus.CORRECT, word);
+    return new Result(Answer.correct(word));
   }
 
-  public static wrong(word?: string) {
-    return new Answer(AnswerStatus.WRONG, word);
+  public static wrong(letters: LetterResult[], word?: string) {
+    return new Result(Answer.wrong(word), letters);
   }
-}
-
-export enum AnswerStatus {
-  CORRECT,
-  WRONG
-}
-
-export enum LetterStatus {
-  CORRECT_POSITION,
-  INCORRECT_POSITION,
-  NOT_PRESENT
 }
 
 export default Result

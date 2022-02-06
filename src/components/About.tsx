180c1e97f7bf1ea2ad89b7b47fd9d3e5fcf5ea75
Pranav Sricharan <pranav.sricharan@gmail.com>
2022-02-06 19:54:28 +0530

    chore: update about section with links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,6 +4,11 @@ import "./About.css";
 export default function About() {
   return (
   <section className="about-content">
-    This is a small React project inspired by the famous Wordle game. This game has no association with the developers of the original game. This is created solely for educational purposes. No copyright infringement intended. Checkout the original game <a href="https://www.powerlanguage.co.uk/wordle/" title="Original Wordle game" target="_blank" rel="noreferrer">here</a>.
+    <p>This is a small React project inspired by the famous Wordle game. This game has no association with the developers of the original game. This is created solely for educational purposes. No copyright infringement intended.</p>
+    <p>
+    Checkout the original game <a href="https://www.powerlanguage.co.uk/wordle/" title="Original Wordle game" target="_blank" rel="noreferrer">here</a>.<br />
+
+    Checkout the code to this react app <a href="https://github.com/pranavsricharan/word-guess-game/" target="_blank" rel="noreferrer">here</a>
+    </p>
   </section>);
 }
